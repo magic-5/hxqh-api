a5ee6303369cd72de55e63ed330737010b233b3f
magic-5 <309507762@qq.com>
2017-09-29 10:08:53 +0800

    创建自定义返回数据的相关实体类

--- src/main/java/com/bjhxqh/web/RoleController.java
+++ src/main/java/com/bjhxqh/web/RoleController.java
@@ -1,80 +1,58 @@
 package com.bjhxqh.web;
 
 
-import com.bjhxqh.common.exception.ResourceNotFoundException;
-import com.bjhxqh.common.util.Constant;
-import com.bjhxqh.common.util.PaginatedResult;
-import com.bjhxqh.mybatis.model.User;
+
+import com.bjhxqh.common.response.Response;
 import com.bjhxqh.mybatis.model.po.SysRole;
 import com.bjhxqh.service.SysRoleService;
-import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
 
 /**
  * Created by Administrator on 2017-07-20.
  */
 
 @RestController
+@RequestMapping("/api/role")
 public class RoleController {
     @Autowired
     private SysRoleService roleService;
 
     @ApiOperation(value = "获取角色列表")
-    @GetMapping("/api/role")
-    public ResponseEntity<?> getList(SysRole role) {
-        //List<SysRole> roleList = roleService.selectByRole(role);
-        //return roleList;
-        return ResponseEntity
-                .ok(new PageInfo<SysRole>(roleService.selectByRole(role)));
+    @GetMapping("/")
+    public Response getList(SysRole role) {
+        return new Response().success(roleService.selectByRole(role));
     }
 
     @ApiOperation(value = "获取角色详情")
-    @GetMapping("/api/role/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public Response getUserById(@PathVariable Long id) {
         SysRole role = new SysRole();
         role.setId(id);
-        return ResponseEntity.ok(roleService
-                .selectByEntity(role));
+        role = roleService.selectByEntity(role);
+        return new Response().success(role);
     }
 
     @ApiOperation(value = "新增角色")
-    @PostMapping("/api/role")
-    public ResponseEntity<?> postUser(@RequestBody SysRole role) {
-        roleService.save(role);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/api/role/{id}")
-                .buildAndExpand(role.getId())
-                .toUri();
-        return ResponseEntity
-                .created(location)
-                .body(role);
+    @PostMapping("/")
+    public Response postUser(@RequestBody SysRole role) {
+        int isExc = roleService.save(role);
+        return new Response().success(role);
     }
 
     @ApiOperation(value = "修改角色")
-    @PutMapping("/api/role/{id}")
-    public ResponseEntity<?> putUser(@PathVariable Long id, @RequestBody SysRole role) {
+    @PutMapping("/{id}")
+    public Response putUser(@PathVariable Long id, @RequestBody SysRole role) {
         role.setId(id);
         roleService.updateNotNull(role);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(role);
+        return new Response().success(role);
     }
 
     @ApiOperation(value = "删除用户")
-    @DeleteMapping("/api/role/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public Response deleteUser(@PathVariable Long id) {
         roleService.delete(id);
-        return ResponseEntity
-                .noContent()
-                .build();
+        return new Response().success(id);
     }
 }
